1f65a20d2ac63be1d7bc88e7eddc6cca908d92b7
Maksim Kalmykov <m.n.kalmykov@gmail.com>
2020-08-22 17:40:51 +0400

    split main command to multiple methods

--- ecs.py
+++ ecs.py
@@ -9,6 +9,14 @@ import click
 from halo import Halo
 from PyInquirer import prompt
 
+ecs_client = boto3.client("ecs")
+ec2_client = boto3.client("ec2")
+
+DEFAULT_PRELOADER_STYLES = {
+    "text_color": "white",
+    "color": "green",
+}
+
 
 @click.group()
 def ecs():
@@ -27,13 +35,25 @@ def ecs():
 def exec(cluster, service, container, user, identity_file, command):
     normalized_command = " ".join(command)
 
-    ecs_client = boto3.client("ecs")
-    ec2_client = boto3.client("ec2")
+    container_task_arns = _get_container_task_arns(cluster, service)
+    container_instance_arns = _get_container_instance_arns(
+        cluster, container_task_arns, container
+    )
+    container_instance_ids = _get_container_instance_ids(
+        cluster, container_instance_arns
+    )
+    container_instance_ips = _get_container_instance_ips(container_instance_ids)
+    target_instance_ip = _ask_target_instance_ip(container_instance_ips)
 
+    _ask_should_exec(normalized_command, container, target_instance_ip)
+
+    return _exec(identity_file, user, target_instance_ip, container, normalized_command)
+
+
+def _get_container_task_arns(cluster, service):
     with Halo(
         text="Obtaining ARNs of running ECS tasks with EC2 launch type",
-        text_color="white",
-        color="green",
+        **DEFAULT_PRELOADER_STYLES,
     ) as sp:
         list_tasks_response = ecs_client.list_tasks(
             cluster=cluster,
@@ -41,24 +61,28 @@ def exec(cluster, service, container, user, identity_file, command):
             desiredStatus="RUNNING",
             launchType="EC2",
         )
-        target_task_arns = list_tasks_response["taskArns"]
+        container_task_arns = list_tasks_response["taskArns"]
 
         sp.succeed()
 
-    if len(target_task_arns) == 0:
+    if len(container_task_arns) == 0:
         click.echo(
             click.style("\nCould not find target tasks. Exiting!", fg="red", bold=True)
         )
         sys.exit(1)
 
-    for task_arn in target_task_arns:
+    for task_arn in container_task_arns:
         click.echo(click.style(f"  - {task_arn}", fg="green"))
 
+    return container_task_arns
+
+
+def _get_container_instance_arns(cluster, task_arns, container):
     with Halo(
-        text="Obtaining container instance ARNs", text_color="white", color="green"
+        text="Obtaining container instance ARNs", **DEFAULT_PRELOADER_STYLES
     ) as sp:
         describe_tasks_response = ecs_client.describe_tasks(
-            cluster=cluster, tasks=target_task_arns,
+            cluster=cluster, tasks=task_arns,
         )
         tasks_with_target_container = [
             t
@@ -74,8 +98,12 @@ def exec(cluster, service, container, user, identity_file, command):
     for instance_arn in container_instance_arns:
         click.echo(click.style(f"  - {instance_arn}", fg="green"))
 
+    return container_instance_arns
+
+
+def _get_container_instance_ids(cluster, container_instance_arns):
     with Halo(
-        text="Obtaining container instance IDs", text_color="white", color="green"
+        text="Obtaining container instance IDs", **DEFAULT_PRELOADER_STYLES
     ) as sp:
         describe_container_instances_response = ecs_client.describe_container_instances(
             cluster=cluster, containerInstances=container_instance_arns,
@@ -92,10 +120,12 @@ def exec(cluster, service, container, user, identity_file, command):
     for instance_id in container_instance_ids:
         click.echo(click.style(f"  - {instance_id}", fg="green"))
 
+    return container_instance_ids
+
+
+def _get_container_instance_ips(container_instance_ids):
     with Halo(
-        text="Obtaining container instance IP addresses",
-        text_color="white",
-        color="green",
+        text="Obtaining container instance IP addresses", **DEFAULT_PRELOADER_STYLES,
     ) as sp:
         describe_instances_response = ec2_client.describe_instances(
             InstanceIds=container_instance_ids,
@@ -115,6 +145,10 @@ def exec(cluster, service, container, user, identity_file, command):
     for instance_ip in container_instance_ips:
         click.echo(click.style(f"  - {instance_ip}", fg="green"))
 
+    return container_instance_ips
+
+
+def _ask_target_instance_ip(container_instance_ips):
     target_instance_ip_answer = prompt(
         [
             {
@@ -125,13 +159,16 @@ def exec(cluster, service, container, user, identity_file, command):
             }
         ]
     )
-    target_instance_ip = target_instance_ip_answer["target_instance_ip"]
 
+    return target_instance_ip_answer["target_instance_ip"]
+
+
+def _ask_should_exec(command, container, instance_ip):
     should_exec_answer = prompt(
         [
             {
                 "type": "confirm",
-                "message": f'You are about to execute "{normalized_command}" in the container "{container}" running on the instance "{target_instance_ip}". Do you want to continue?',
+                "message": f'You are about to execute "{command}" in the container "{container}" running on the instance "{instance_ip}". Do you want to continue?',
                 "name": "should_exec",
                 "default": True,
             }
@@ -141,12 +178,12 @@ def exec(cluster, service, container, user, identity_file, command):
 
     if not should_exec:
         click.echo(click.style("\nOperation aborted. Exiting!", fg="red", bold=True))
-        return
+        sys.exit(0)
 
-    click.echo()
 
+def _exec(identity_file, user, instance_ip, container, command):
     return subprocess.call(
-        f"ssh -i {identity_file} {user}@{target_instance_ip} -t 'bash -c \"docker exec -it $(docker ps -q -f name='-{container}-' | head -n 1) {normalized_command}\"'",
+        f"ssh -i {identity_file} {user}@{instance_ip} -t 'bash -c \"docker exec -it $(docker ps -q -f name='-{container}-' | head -n 1) {command}\"'",
         shell=True,
     )
 
