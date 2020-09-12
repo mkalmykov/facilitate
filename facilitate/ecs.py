0a821b954bd5cdd34a27bf9482de31ef578c039f
Maksim Kalmykov <m.n.kalmykov@gmail.com>
2020-09-12 08:23:17 +0400

    fix container detection query

--- facilitate/ecs.py
+++ facilitate/ecs.py
@@ -1,17 +1,3 @@
-"""
-Copyright 2020 Maksim Kalmykov
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
 import subprocess
 import sys
 from itertools import chain
@@ -179,7 +165,7 @@ def _ask_should_exec(command, container, instance_ip):
 
 def _exec(identity_file, user, instance_ip, container, command):
     return subprocess.call(
-        f"ssh -i {identity_file} {user}@{instance_ip} -t 'bash -c \"docker exec -it $(docker ps -q -f name='-{container}-' | head -n 1) {command}\"'",
+        f"ssh -i {identity_file} {user}@{instance_ip} -t 'bash -c \"docker exec -it $(docker ps -q -f name='{container}' | head -n 1) {command}\"'",
         shell=True,
     )
 
